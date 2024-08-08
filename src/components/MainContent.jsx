cd33eff0865868e16c06893eecfc78da1b851f67
Semight <davidoluwasemiloorei@gmail.com>
2024-08-08 23:59:16 +0100

    code modified

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -13,28 +13,28 @@ const MainContent = () => {
     <main className="bg-[#F5F5F5] h-full">
       <div className="px-4 pt-8 pb-4">
         <div className="flex justify-between items-center">
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={newPost} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">New Post</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={group} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">QnAs</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={news} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">Kiwi</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={eyije} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">Rands</p>
@@ -47,28 +47,28 @@ const MainContent = () => {
         </div>
 
         <div className="flex justify-between items-center">
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={vector} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">New Post</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={diary} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">QnAs</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={owambe} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">Kiwi</p>
             </div>
           </div>
 
-          <div className="border-gray-400 bg-white border rounded-xl h-20 w-20">
+          <div className="border-gray-400 bg-white border rounded-xl h-[78px] w-[78px]">
             <div className="flex justify-center items-center flex-col gap-1 pt-4">
                 <img src={streaks} alt="" className="w-6 h-6" />
                 <p className="text-sm text-[#4F4F4F] font-normal">Rands</p>
